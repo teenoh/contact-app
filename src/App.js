ae959d1047935582f88a6bb83bf2cc622417a2d6
teenoh <valentino@grit.systems>
2017-08-03 06:00:56 +0100

    completed mockup for contact-list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
 
-import {BrowserRouter as Router,
-        Route,
-        Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Contact_lists from './components/Contact_lists'
-import Navi from './components/Navi'
-import Login from './components/Login'
+import Contact_lists from "./components/Contact_lists";
+import Navi from "./components/Navi";
+import Login from "./components/Login";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
+        <div className="container-fluid">
           <Navi />
-          <Switch>
-            <Route exact path='/' component={Contact_lists} />
-            <Route exact path='/login' component={Login} />
-            <Route render={() => <p>Page not Found!!</p>} />          
-          </Switch>
-        </div> 
-       </Router> 
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Contact_lists} />
+              <Route exact path="/login" component={Login} />
+              <Route render={() => <p>Page not Found!!</p>} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,29 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="col-sm-4">
+        <div className="card">
+          <img className="card-img-top" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"/>
+          <div className="card-block">
+            <h4 className="card-title">Card title</h4>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+          <div className="card-footer">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
